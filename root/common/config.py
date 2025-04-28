6c1306232263a5c1ebe66401a11f00f12d8e74c5
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-28 13:21:44 +0330

    Delete root/common directory

--- root/common/config.py
+++
@@ -1,9 +0,0 @@
-BOT_TOKEN_MAIN = "توکن ربات اصلی"
-BOT_TOKEN_CHECKER = "توکن ربات چکر"
-
-OWNER_ID = 123456789  # آیدی عددی مدیر ربات
-
-CHANNEL_ID = -1001234567890  # آیدی عددی کانال (حتما با -100 شروع بشه)
-CHANNEL_LINK = "https://t.me/YourChannelLink"
-
-CHECKER_BOT_USERNAME = "checker_bot_username"  # بدون @
